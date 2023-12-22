aeb8799fc90c13342b167d57003013b6a8fe9516
Gustavo <gustavo.gevigi@al.infnet.edu.br>
2023-12-21 21:45:15 -0300

    Loja 1.0 PB

---
+++ TP 1/HostBuilder/Services/ConfigureApplicationCookie.cs
@@ -0,0 +1,6 @@
+﻿namespace HostBuilder.Services
+{
+    internal class ConfigureApplicationCookie
+    {
+    }
+}
---
+++ TP 1/Pages/ProdutosView.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TP_1.Models;
+using TP_1.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP_1.Pages
+{
+    public class ProdutosView : PageModel
+    {
+        private readonly TP_1.Data.TP_1Context _context;
+
+        public ProdutosView(TP_1.Data.TP_1Context context)
+        {
+            _context = context;
+        }
+
+        public ProdutosData Produto { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.ProdutosData == null)
+            {
+                return NotFound();
+            }
+
+            var produto = await _context.ProdutosData.FirstOrDefaultAsync(m => m.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Produto = produto;
+            }
+            return Page();
+        }
+    }
+}
